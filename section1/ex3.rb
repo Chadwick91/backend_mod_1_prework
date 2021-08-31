843f2bec95abacb56c2efd60d7c3d1cee7429f38
Chad Morger <chadmorger@gmail.com>
2021-08-30 21:20:44 -0600

    Add math equations

---
+++ section1/ex3.rb
@@ -0,0 +1,25 @@
+# simple puts statement that will print the string
+puts "I will now count my chickens:"
+# puts Hens, calculates: 25 plus 30 divided by 6
+puts "Hens #{25 + 30 / 6}"
+# puts Roosters, calculates: 25 times 3, 100 minus 75, modulo 4
+puts "Roosters #{100 - 25 * 3 % 4}"
+# simple puts statement that will print the string
+puts "Now I will count the eggs:"
+#
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
