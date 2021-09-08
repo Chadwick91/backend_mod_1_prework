2792b617240713c2b2042f08a1eeb991a550a176
Chad Morger <chadmorger@gmail.com>
2021-09-08 17:03:40 -0600

    Complete say exercise

--- section2/exercises/say.rb
+++ section2/exercises/say.rb
@@ -1,8 +1,16 @@
-def say(words)
+def say(words='hello')
   puts words + '.'
 end
 
-say("Hello")
+say()
 say("hi")
 say("how are you")
 say("I'm fine")
+
+a = 5
+
+def some_method
+  a = 3
+end
+
+puts a 
