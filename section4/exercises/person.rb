0a01bb75ce6957e612a0f48a21928257767894d4
Chad Morger <chadmorger@gmail.com>
2021-09-09 10:56:19 -0600

    Complete exercise

--- section4/exercises/person.rb
+++ section4/exercises/person.rb
@@ -1,5 +1,29 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_accessor :first_name, :last_name, :gender
+  def initialize(first_name, last_name, gender)
+    @first_name = first_name
+    @last_name = last_name
+    @gender = gender
+  end
+
+  def activity
+    "#{@first_name} loves to run!"
+  end
+
+  def eat
+    "#{@first_name} loves to eat tacos!"
+  end
+end
+
+ryan = Person.new("Ryan", "Perez", "Male")
+
+p ryan.first_name
+p ryan.last_name
+p ryan.gender
+
+p ryan.activity
+p ryan.eat
